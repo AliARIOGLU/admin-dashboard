792de927feaf16172cf681d6d42d0aa929cf429a
Ali <ali.arioglu06@gmail.com>
2023-07-11 22:39:50 +0300

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Codex Dashboard</span>
+      <span>© Ali Arioglu Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
